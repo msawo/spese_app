083ca066b65a8d0fd33f13c1b76e8b7262a5ba49
msawo <msawo.inc@gmail.com>
2020-06-05 23:00:08 -0400

    Basic setup, placeholders for chart and TX list

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ class SpeseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spese App',
       home: HomePage(),
     );
   }
@@ -20,8 +22,38 @@ class HomePage extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return CupertinoPageScaffold(
-      child: Text('Welcome'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Flexible(
+              child: Card(
+                child: Container(
+                  width: double.infinity,
+                  child: Text('CHART', style: TextStyle(fontSize: 19)),
+                ),
+                elevation: 5,
+                color: Colors.blueGrey.shade300,
+              ),
+              flex: 3,
+            ),
+            Flexible(
+              child: Card(
+                child: Container(
+                  child: Text(
+                    'Tansaction list',
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+                color: Colors.blueGrey.shade800,
+              ),
+              flex: 7,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
