82d434ad8a83d60dc20fb92977fb9cbcd7ea1c59
msawo <msawo.inc@gmail.com>
2020-06-07 22:28:32 -0400

    Lay out the scaffold, style it

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import './transaction.dart';
 import 'package:flutter/services.dart';
+import './common.dart';
 
 void main() => runApp(SpeseApp());
 
@@ -16,6 +18,21 @@ class SpeseApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 'n1',
+      amount: 187.98,
+      date: DateTime.now(),
+      title: 'BURBERRY The City Silver Dial Haymarket Men\'s Watch',
+    ),
+    Transaction(
+      id: 'n2',
+      amount: 350.14,
+      date: DateTime.now(),
+      title: 'Stock purchase on Global Acct.',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -28,11 +45,16 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            // TODO: display the list of TX
             Flexible(
               child: Card(
                 child: Container(
                   width: double.infinity,
-                  child: Text('CHART', style: TextStyle(fontSize: 19)),
+                  child: Text('CHART',
+                      style: TextStyle(
+                        fontSize: 19,
+                        color: primaryColor,
+                      )),
                 ),
                 elevation: 5,
                 color: Colors.blueGrey.shade300,
@@ -40,16 +62,62 @@ class HomePage extends StatelessWidget {
               flex: 3,
             ),
             Flexible(
-              child: Card(
-                child: Container(
-                  child: Text(
-                    'Tansaction list',
-                    style: TextStyle(fontSize: 19),
-                  ),
-                ),
-                color: Colors.blueGrey.shade800,
-              ),
               flex: 7,
+              child: Column(
+                children: _transactions.map((tx) {
+                  return Card(
+                      child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          '\$ ${tx.amount}',
+                          style: TextStyle(
+                            color: white,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 15.0,
+                        ),
+                        decoration: BoxDecoration(
+                          color: accentColor,
+                          border: Border.all(
+                            color: darkPrimaryColor,
+                            width: 2.0,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10.0,
+                          horizontal: 15.0,
+                        ),
+                        constraints: BoxConstraints(maxWidth: 100,minWidth: 100),
+                      ),
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style:
+                                  TextStyle(color: primaryText, fontSize: 16),
+                            ),
+                            Text(
+                              tx.date.toString(),
+                              style: TextStyle(
+                                color: secondaryText,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ));
+                }).toList(),
+              ),
             ),
           ],
         ),
