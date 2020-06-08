51709a783f6f83244834e06e2348e6b1926095a5
msawo <msawo.inc@gmail.com>
2020-06-07 23:49:24 -0400

    Create common widget for colors, etc

---
+++ lib/common.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const accentColor = Color(0xffE040FB);
+const primaryColor = Color(0xff9C27B0);
+const lightPrimaryColor = Color(0xffE1BEE7);
+const darkPrimaryColor = Color(0xff7B1FA2);
+const white = Colors.white;
+const dividerColor = Color(0xffBDBDBD);
+const primaryText = Color(0xff212121);
+const secondaryText = Color(0xff757575);
