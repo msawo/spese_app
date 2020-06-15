3a0c3da86981bcc38f5c207b22f53170bda0d325
msawo <msawo.inc@gmail.com>
2020-06-15 01:08:10 -0400

    Add img for waiting period b4 tx input

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,67 +13,92 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
-                Container(
-                  child: Text(
-                    '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      color: white,
-                      fontSize: 13.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  margin: EdgeInsets.symmetric(
-                    vertical: 14.0,
-                    horizontal: 15.0,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30.0),
-                    color: Theme.of(context).primaryColor,
-                    border: Border.all(
-                      color: Theme.of(context).primaryColorDark,
-                      width: 1.0,
-                    ),
-                  ),
-                  padding: EdgeInsets.symmetric(
-                    vertical: 8.0,
-                    horizontal: 8.0,
-                  ),
-                  constraints: BoxConstraints(maxWidth: 100, minWidth: 100),
+                Text(
+                  '\nNo Transactions added yet',
+                  style: Theme.of(context).textTheme.title,
                 ),
-                Expanded(
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        Text(
-                          DateFormat.yMMMMd("en_US")
-                              .format(transactions[index].date),
-                          style: TextStyle(
-                            color: secondaryText,
-                            fontSize: 14,
-                            height: 1.4,
-                          ),
-                        ),
-                      ],
-                    ),
+                SizedBox(
+                  height: 15.0,
+                  width: 300.0,
+                  child: Divider(
+                    thickness: 0.5,
+                    color: dividerColor,
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
               ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            color: white,
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 14.0,
+                          horizontal: 15.0,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30.0),
+                          color: Theme.of(context).primaryColor,
+                          border: Border.all(
+                            color: Theme.of(context).primaryColorDark,
+                            width: 1.0,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 8.0,
+                        ),
+                        constraints:
+                            BoxConstraints(maxWidth: 100, minWidth: 100),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                transactions[index].title,
+                                style: Theme.of(context).textTheme.title,
+                              ),
+                              Text(
+                                DateFormat.yMMMMd("en_US")
+                                    .format(transactions[index].date),
+                                style: TextStyle(
+                                  color: secondaryText,
+                                  fontSize: 14,
+                                  height: 1.4,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
